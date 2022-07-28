0be32c040e7c15ff9e45c976f6eacdf0c065901b
ABDURSHAZAM <85063702+ABDURSHAZAM@users.noreply.github.com>
2022-07-28 17:59:34 +0100

    CustomerTest first pass

---
+++ src/test/java/com/sparta/northwind/controllers/CustomerControllerTest.java
@@ -0,0 +1,31 @@
+package com.sparta.northwind.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.northwind.entities.Customer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerControllerTest {
+
+    @Test
+    void checkCustomerExists(){
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            Customer result = mapper.readValue(
+                    new URL("http://localhost:8080/customers/1"),Customer.class);
+            Assertions.assertNotEquals(0, result.getPhone());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+      @Test
+    void extraDummyTest(){
+        Assertions.assertEquals(0,0);
+    }
+
+}
